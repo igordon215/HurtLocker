759c14842d906097f3422a03fbbc3930379d6dac
igordon215 <igordon215@gmail.com>
2024-08-15 09:48:45 -0400

    cant use stringbuilder ItemFormatter

--- src/main/java/ItemFormatter.java
+++ src/main/java/ItemFormatter.java
@@ -4,31 +4,62 @@ import java.util.List;
 import java.util.Map;
 
 public class ItemFormatter {
+
     public String formatItem(Map<String, String> item) {
-        StringBuilder formattedItem = new StringBuilder();
-        List<String> keys = new ArrayList<>(item.keySet());
-        keys.sort(String::compareTo);
-        for (String key : keys) {
-            String value = item.get(key);
-            if (key.equalsIgnoreCase("name")) {
-                value = formatName(value);
-            }
-            formattedItem.append(capitalize(key)).append(": ").append(value).append("\n");
-        }
-        return formattedItem.toString();
+        return null;
     }
 
-    private String formatName(String name) {
-        String matchedName = PatternMatcher.findMatchingPattern(name);
-        return capitalize(matchedName != null ? matchedName : name);
-    }
 
-    private String capitalize(String str) {
-        if (str == null || str.isEmpty()) {
-            return str;
-        }
-        return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //README- CANT USE STRINGBUILDER....
+    //    public String formatItem(Map<String, String> item) {
+//        StringBuilder formattedItem = new StringBuilder();
+//        List<String> keys = new ArrayList<>(item.keySet());
+//        keys.sort(String::compareTo);
+//        for (String key : keys) {
+//            String value = item.get(key);
+//            if (key.equalsIgnoreCase("name")) {
+//                value = formatName(value);
+//            }
+//            formattedItem.append(capitalize(key)).append(": ").append(value).append("\n");
+//        }
+//        return formattedItem.toString();
+//    }
+//    // USE PATTERN MATCHER CLASS TO - FORMAT FOOD NAMES
+//    private String formatName(String name) {
+//        String matchedName = PatternMatcher.findMatchingPattern(name);
+//        return capitalize(matchedName != null ? matchedName : name);
+//    }
+//    // CAPITALIZE ALL FIRST LETTERS OF WORDS
+//    private String capitalize(String str) {
+//        if (str == null || str.isEmpty()) {
+//            return str;
+//        }
+//        return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+//    }
 
 
 //    public static void main(String[] args) {
